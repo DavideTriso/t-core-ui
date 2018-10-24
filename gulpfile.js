70a576f0b5684ce4cfb73cce3aedfb4c9f8f92f6
DavideTriso <davidetrisolinI@yahoo.it>
2018-10-24 22:53:05 +0200

    downgraded to gulp v3.9. Added modif. for .txt

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,37 @@
 //Require packages
 var gulp = require('gulp'),
-  sass = require('gulp-ruby-sass'),
+  sass = require('gulp-sass'),
   autoprefixer = require('gulp-autoprefixer'),
   cssnano = require('gulp-cssnano'),
   rename = require('gulp-rename'),
   notify = require('gulp-notify');
+sass.compiler = require('node-sass');
 
 //SCSS -> CSS
 //Compile SCSS and save compiled + minified CSS file to 'dist' folder
-gulp.task('scssDist', function (done) {
-  sass('src/**/*.scss', { style: 'expanded' }).pipe(autoprefixer('last 5 version')).pipe(gulp.dest('dist')).pipe(rename({ suffix: '.min' })).pipe(cssnano()).pipe(gulp.dest('dist')).pipe(notify({ message: 'scssDist task complete' }));
-  done();
+gulp.task('scssDist', function () {
+  return gulp.src('./src/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer('last 5 version'))
+    .pipe(gulp.dest('./dist'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(cssnano())
+    .pipe(gulp.dest('./dist'))
+    .pipe(notify({ message: 'scssDist task complete' }));
 });
 
 //Save a copy of compiled CSS in 'docs' folder
-gulp.task('scssDocs', function (done) {
-  sass('src/**/*.scss', { style: 'expanded' }).pipe(autoprefixer('last 5 version')).pipe(gulp.dest('docs')).pipe(rename({ suffix: '.min' })).pipe(notify({ message: 'scssDocs task complete' }));
-  done();
-});
-
-//Task alias
-gulp.task('scss', function (done) {
-  gulp.parallel('scssDist', 'scssDocs');
-  done();
+gulp.task('scssDocs', function () {
+  return gulp.src('./src/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer('last 5 version'))
+    .pipe(gulp.dest('./docs'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(notify({ message: 'scssDocs task complete' }));
 });
 
 //WATCH TASKS
-
-gulp.task('watchScss', function () {
-  gulp.watch('src/**/*.scss', gulp.parallel('scss'));
+gulp.task('watchScss', function (done) {
+  gulp.watch('./src/**/*.scss', ['scssDist', 'scssDocs']);
+  done();
 });
